59839061d6e07ee8f34f307bf02b2d86093846ac
Anil prajapati <its.anilprajapatii@gmail.com>
2024-02-26 22:07:09 +0530

    worked on config, constants and models and established mongodb connection

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,45 +1,51 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 //@desc get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req, resp) => {
-  resp.status(200).json({ message: "get all contacts" });
-};
+const getContacts = asyncHandler(async (req, resp) => {
+  const contacts = await Contact.find();
+  resp.status(200).json(contacts);
+});
 
 //@desc Create New contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, resp) => {
-  
-  const {name,email,phone} = req.body;
-  if(!name||!email||!phone){
-    console.log("EVERY filed is mandatory !");
+const createContact = asyncHandler(async (req, resp) => {
+  console.log("Request BODY is ", req, body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    resp.status(400);
+    throw new Error("All Fileds Are Mandotry !");
   }
-  else{
-    console.log("the BODY ",req.body);
-  }
-  resp.status(201).json({ message: "Create contacts" });
-};
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  resp.status(201).json(contact);
+});
 
 //@desc GET contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, resp) => {
+const getContact = asyncHandler(async (req, resp) => {
   resp.status(200).json({ message: `get contact for ${req.params.id}` });
-};
+});
 
 //@desc PUT contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = (req, resp) => {
+const updateContact = asyncHandler(async (req, resp) => {
   resp.status(200).json({ message: `Update contacts for ${req.params.id}` });
-};
+});
 
 //@desc Delete contact
 //@route delete /api/contacts/:id
 //@access public
-const deleteContact = (req, resp) => {
+const deleteContact = asyncHandler(async (req, resp) => {
   resp.status(200).json({ message: `Delete contacts for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
--- mycontacts-backend/middleware/errorHandler.js
+++ mycontacts-backend/middleware/errorHandler.js
@@ -1,3 +1,46 @@
-const errorHandler = (err,req,resp,next) =>{}
+const {constants} = require("../constants")
+const errorHandler = (err, req, resp, next) => {
+  const statusCode = resp.statusCode ? resp.statusCode : 500;
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      resp.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.NOT_FOUND:
+      resp.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+      case constants.UNAUTHORIZED:
+      resp.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+      case constants.FORBIDDEN:
+      resp.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      case constants.SERVER_ERROR:
+      resp.json({
+        title: "Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    default:
+        console.log("No Errros, All Good !");
+      break;
+  }
+};
 
 module.exports = errorHandler;
