e941e10a0385e409eb6d2786c95ed819303f16de
Anil prajapati <its.anilprajapatii@gmail.com>
2024-02-27 08:16:59 +0530

    Added functionalities for CRUD operations from the  database

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -12,7 +12,6 @@ const getContacts = asyncHandler(async (req, resp) => {
 //@route POST /api/contacts
 //@access public
 const createContact = asyncHandler(async (req, resp) => {
-  console.log("Request BODY is ", req, body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     resp.status(400);
@@ -30,21 +29,43 @@ const createContact = asyncHandler(async (req, resp) => {
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async (req, resp) => {
-  resp.status(200).json({ message: `get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    resp.status(404);
+    throw new Error("Contact not found !");
+  }
+  resp.status(200).json(contact);
 });
 
 //@desc PUT contact
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, resp) => {
-  resp.status(200).json({ message: `Update contacts for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    resp.status(404);
+    throw new Error("Contact not found !");
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id, 
+    req.body, 
+    {new: true}
+    );
+    resp.status(200).json(updatedContact);
 });
 
+
 //@desc Delete contact
-//@route delete /api/contacts/:id
+//@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, resp) => {
-  resp.status(200).json({ message: `Delete contacts for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    resp.status(404);
+    throw new Error("Contact not found !");
+  }
+  await Contact.deleteOne({ _id: req.params.id });
+  resp.status(200).json({ message: "Contact deleted successfully",contact });
 });
 
 module.exports = {
